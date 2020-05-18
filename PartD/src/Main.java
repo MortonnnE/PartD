07b3997aad23464b726b46d01c96ac8e7a362f0e
MortonnnE <jackster95@hotmail.com>
2020-05-18 13:33:31 +1200

    Transportation added

--- PartD/src/Main.java
+++ PartD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		
 		System.out.println("Hi, my name is Jack");
 		System.out.println("My favourite colour is blue");
+		System.out.println("My favourite mode of transportation is car");
 	}
 
 }
